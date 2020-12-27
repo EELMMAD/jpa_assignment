45d0a125b798c1cc0e2a7100ef42ed40bd8c85c6
Elmira Madadi <elmiramadadi@gmail.com>
2020-12-27 12:10:27 +0100

    Created ingredient entity and test

---
+++ jpa_assignment/src/main/java/se/lexicon/elmira/jpa_assignment/entity/Ingredient.java
@@ -0,0 +1,61 @@
+package se.lexicon.elmira.jpa_assignment.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+
+
+public class Ingredient {
+
+
+       private int ingredient_id;
+     private String ingredient_name;
+
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String ingredient_name) {
+
+        this.ingredient_name = ingredient_name;
+    }
+
+    public int getIngredient_id() {
+        return ingredient_id;
+    }
+
+
+    public String getIngredient_name() {
+        return ingredient_name;
+    }
+
+    public void setIngredient_name(String ingredient_name) {
+        this.ingredient_name = ingredient_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "ingredient_id=" + ingredient_id +
+                ", ingredient_name='" + ingredient_name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return ingredient_id == that.ingredient_id &&
+                Objects.equals(ingredient_name, that.ingredient_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredient_id, ingredient_name);
+    }
+}
