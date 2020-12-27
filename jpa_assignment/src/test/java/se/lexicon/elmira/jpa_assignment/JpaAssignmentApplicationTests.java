66d68f29f9f57e4f9b24287747676438e9845631
Elmira Madadi <elmiramadadi@gmail.com>
2020-12-27 14:52:50 +0100

    added application and test.

---
+++ jpa_assignment/src/test/java/se/lexicon/elmira/jpa_assignment/JpaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.elmira.jpa_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
